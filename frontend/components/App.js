931dc63dfa7b679e68d7a3c7cd77f30c87b6e212
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-04-12 21:50:11 -0500

    fetching api

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [],
+    }
+    
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({ ...this.state, todos: res.data.data })
+    })
+    .catch(err => console.error(err))
+  }
+componentDidMount() {
+  this.fetchAllTodos()
+  
+}
+  
   render() {
-    return null
+    return (
+      <div>
+      <h1>Todos:</h1>
+        <ul>
+          {
+            this.state.todos.map(todo => {
+              return <li key={todo.id}>{todo.name}</li>
+            })
+          }
+        </ul>
+        <form>
+        <input />
+          <button>Add</button>
+          </form>
+          <button>Clear Completed</button>
+        </div>
+       
+    )
   }
 }
